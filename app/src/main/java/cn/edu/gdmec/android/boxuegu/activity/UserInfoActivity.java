10fddd8708c995dbca430b698adeec9cff7c9498
huangguangda <2397923107@qq.com>
2017-12-27 13:24:27 +0800

    完善我的界面OK

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/UserInfoActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/UserInfoActivity.java
@@ -30,6 +30,7 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
     private RelativeLayout rl_signature;
     private String spUserName;
     private String sex;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
