dc6ddfa185c086322f00b79bb0a69567f4ae16aa
huangguangda <2397923107@qq.com>
2018-01-02 18:35:18 +0800

    ok

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
@@ -15,6 +15,13 @@ import cn.edu.gdmec.android.boxuegu.R;
 
 /**
  * Created by Jack on 2017/12/26.
+ * 1.获取版本号
+ * 在SplashActivity中创建init()方法，在方法中获取TextView控件并通过PackageManager
+ * (包管理器)，获取层序版本号
+ * （版本号是build.gradle文件中的versionName的值显示在TextView控件上。
+ * 2.让界面延迟跳转
+ * 在init()方法中使用Timer以及TimerTask类设置欢迎界面延迟3秒在跳转到主界面
+ *
  */
 
 public class SplashActivity extends AppCompatActivity{
@@ -28,9 +35,14 @@ public class SplashActivity extends AppCompatActivity{
         init();
     }
     private void init(){
+        //添加版本控件
         tv_version=(TextView) findViewById ( R.id.tv_version );
         try{
          //获取程序包信息
+            //首先通过 PackageManager 的 getPackageInfo()方法获取 PackageInfo 对象
+            //通过该对象的 versionName 属性获取到程序的版本号
+            //最后通过 setText()方法将获取到的版本号设
+            //置到 TextView 控件上
             PackageInfo info = getPackageManager ().getPackageInfo ( getPackageName (), 0 );
             tv_version.setText ( "V"+info.versionName );
         }catch (PackageManager.NameNotFoundException e){
@@ -38,7 +50,7 @@ public class SplashActivity extends AppCompatActivity{
             tv_version.setText ( "V" );
         }
 
-        //利用Timer让此界面延迟3秒后再跳转，timer中有一个线层，这个线层不断执行task
+        //利用Timer让此界面延迟3 秒后再跳转，timer中有一个线层，这个线层不断执行task
         Timer timer = new Timer (  );
         //timertask 实现runnable接口，TimerTask类表示一个在指定时间内执行的task
         TimerTask task = new TimerTask () {
